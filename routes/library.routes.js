3b1d6106d355d0ac7680cfdee43112528ba5b9e7
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2025-09-05 23:48:39 +0530

    importing controllers to routes

--- routes/library.routes.js
+++ routes/library.routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
+const {BooksRead , BooksCreate , BooksUpdate , BooksDelete} = require("./contollers/library.controllers.js");
+
 
 
 router.get("/" ,BooksRead);
