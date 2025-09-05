c238b647e2b396ea1ae99dac50a002738d9ae8b0
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2025-09-05 23:11:40 +0530

    Created the routes for performing get,post,put,delete operations

---
+++ routes/library.routes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/" , (req , res) => {
+res.send("Books read Successfully");
+});
+
+router.post("/",(req , res ) => {
+res.send("Books created successfully");
+});
+
+router.put("/:id" , (req , res) =>{
+res.send("Books updated successfully");
+});
+
+router.delete("/:id" , (req , res) => {
+res.send("Books deleted successfully");
+});
