567ff4c52c0494b4980aafd895fd5d314ceed584
Priyadharshini-6383 <pm.priyadharshini123@gmail.com>
2025-09-05 22:57:05 +0530

    Imported express and run the code by usin app.get and app.listen

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-//ignore nodemodule
+const express = require("express");
+const app = express();
+const PORT = 5000;
 
-node_modules;
+app.get("/" , (req , res ) => {
+res.send(" Welcome to Library Management System");
+});
+
+
+app.listen(PORT , () => {
+    console.log(`The server will be run on http://localhost:${PORT}`);
+});
